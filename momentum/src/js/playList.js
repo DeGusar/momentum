983447a27dde922d07f10ccc7194edf227f34a72
Denis Gusar <degusar.wot@gmail.com>
2021-10-25 00:55:12 +0300

    fix: change path

--- momentum/src/js/playList.js
+++ momentum/src/js/playList.js
@@ -1,22 +1,22 @@
 const playList = [
     {      
       title: 'Dion Town',
-      src: '../sounds/Dion_town.mp3',
+      src: './sounds/Dion_town.mp3',
       duration: '02:06'
     },  
     {      
         title: 'Gludio Town',
-      src: '../sounds/Gludio_town.mp3',
+      src: './sounds/Gludio_town.mp3',
       duration: '02:01'
     },
     {      
         title: 'Hunters Village',
-      src: '../sounds/Hunters_Village.mp3',
+      src: './sounds/Hunters_Village.mp3',
       duration: '02:17'
     },
     {
         title: 'Talking Island',
-      src: '../sounds/Island_Village.mp3',
+      src: './sounds/Island_Village.mp3',
       duration: '02:04'
     }
 ]
